ba445a3b1780e776edf497df33c160ad2ee79eaf
Dylan Michael Labat <dml252@live.com>
2019-05-09 10:52:28 -0500

    Create deck migration

---
+++ db/migrate/20190509155051_create_decks_table.rb
@@ -0,0 +1,9 @@
+class CreateDecksTable < ActiveRecord::Migration
+  def change
+    create_table :decks do |t|
+      t.string :name
+      t.string :format
+      t.string :colors
+      t.string :decklist
+  end
+end
