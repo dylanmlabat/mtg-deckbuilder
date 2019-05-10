47c4934f95d74aa3bbe3b81a5d3092e2aadbade0
Dylan Michael Labat <dml252@live.com>
2019-05-09 20:41:25 -0500

    Add application controller helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
+
 end
