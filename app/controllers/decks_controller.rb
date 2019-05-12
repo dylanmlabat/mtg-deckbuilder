277f5deecf6d3a649d531a08e5f97c822e932896
Dylan Michael Labat <dml252@live.com>
2019-05-12 11:30:51 -0500

    Add new deck erb and post deck route

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -8,4 +8,24 @@ class DecksController < ApplicationController
     end
   end
 
+  get '/decks/new' do
+    if logged_in?
+      erb :'decks/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/decks' do
+    if params[:name].empty? || params[:format].empty? ||
+       params[:colors].empty? || params[:decklist].empty? ||
+       !logged_in
+      redirect '/decks/new'
+    else
+      @deck = Deck.create(params[:name], params[:format], params[:colors], params[:decklist])
+      @deck.user = params[:username]
+      @deck.save
+    end
+  end
+
 end
