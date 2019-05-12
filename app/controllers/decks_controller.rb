7880ba3ba896bbd26ea50d497957b17552717036
Dylan Michael Labat <dml252@live.com>
2019-05-12 11:10:44 -0500

    Add decks erb and login/signup redirects

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,3 +1,11 @@
 class DecksController < ApplicationController
 
+  get '/decks' do
+    if logged_in?
+      erb :'/decks/decks'
+    else
+      redirect '/login'
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     else
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
+      redirect '/decks'
     end
   end
 
@@ -25,8 +26,18 @@ class UsersController < ApplicationController
     @user = User.find_by(params[:id])
     if @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      redirect '/decks'
     else
       redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/'
+    else
+      redirect '/'
     end
   end
 
