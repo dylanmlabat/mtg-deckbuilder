33be5a754379322d4a8dc1f32d887564adb873a2
Dylan Michael Labat <dml252@live.com>
2019-05-17 00:18:14 -0500

    Add deck slug to deck urls

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -15,55 +15,55 @@ class DecksController < ApplicationController
       redirect '/decks/new'
     else
       @deck = Deck.create(name: params[:name], format: params[:format], colors: params[:colors], decklist: params[:decklist])
-      @deck.user = User.find_by(params[:id])
+      @deck.user = User.find_by_id(params[:id])
       @deck.save
-      redirect "/decks/#{@deck.id}"
+      redirect "/decks/#{@deck.slug}"
     end
   end
 
-  get '/decks/:id' do
+  get '/decks/:slug' do
     if logged_in?
-      @deck = Deck.find_by_id(params[:id])
+      @deck = Deck.find_by_slug(params[:slug])
       erb :'/decks/show'
     else
       redirect '/login'
     end
   end
 
-  get '/decks/:id/edit' do
+  get '/decks/:slug/edit' do
     if logged_in?
-      @deck = Deck.find_by_id(params[:id])
+      @deck = Deck.find_by_slug(params[:slug])
       erb :'/decks/edit'
     else
       redirect '/login'
     end
   end
 
-  post '/decks/:id' do
-    @deck = Deck.find_by_id(params[:id])
+  post '/decks/:slug' do
+    @deck = Deck.find_by_slug(params[:slug])
     if params[:name].empty? || params[:format].empty? ||
        params[:colors].empty? || params[:decklist].empty? ||
        !logged_in?
-      redirect "/decks/#{@deck.id}/edit"
+      redirect "/decks/#{@deck.slug}/edit"
     else
       if @deck.user == current_user
         @deck.update(name: params[:name], format: params[:format], colors: params[:colors], decklist: params[:decklist])
         @deck.save
-        redirect "/decks/#{@deck.id}"
+        redirect "/decks/#{@deck.slug}"
       else
-        redirect "/decks/#{@deck.id}"
+        redirect "/decks/#{@deck.slug}"
       end
     end
   end
 
-  post '/decks/:id/delete' do
+  post '/decks/:slug/delete' do
     if logged_in?
-      @deck = Deck.find_by_id(params[:id])
+      @deck = Deck.find_by_slug(params[:slug])
       if @deck.user == current_user
         @deck.delete
         redirect "/#{current_user.slug}/account"
       else
-        redirect "/decks/#{@deck.id}"
+        redirect "/decks/#{@deck.slug}"
       end
     else
       redirect '/login'
